02482a60ad40274e71bec4271c971af71f50b87f
tianhao250 <105092897+tianhao250@users.noreply.github.com>
2023-03-01 08:52:38 +0800

    Update text.java

--- text.java
+++ text.java
@@ -1,4 +1,5 @@
 public class text{
     public static void main(String arg[]){
-        System.out.println("Verion2.0杨天昊);
+        System.out.println("Verion1.0");
+        System.out.println("Verion2.0杨天昊");
     }}
