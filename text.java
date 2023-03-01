7fb85a024086818826241893a3d3aa17e91748c1
tianhao250 <105092897+tianhao250@users.noreply.github.com>
2023-03-01 08:51:48 +0800

    Update text.java

--- text.java
+++ text.java
@@ -1,5 +1,7 @@
 public class text{
     public static void main(String arg[]){
+        System.out.println("Version1.0");
+          System.out.println("Version2.0杨天昊");
         System.out.println("Version3.0黎骐鸣");
     }
 }
